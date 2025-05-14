725d38b62393e28d648cd487379a47a4cb43acc7
jakubek16cm <kuba.stanczyk201@gmail.com>
2025-05-14 14:38:05 +0000

    6_3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,8 +14,12 @@ Poniższe zadania będą się sprowadzały do modyfikacji bazowego kodu. Proces 
 import java.util.Collection;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 class WrongStudentName extends Exception { }
+class WrongAge extends Exception { }
+class WrongDate extends Exception { }  
+class WrongMenu extends Exception{ }
 
 class Main {
     public static Scanner scan = new Scanner(System.in);
@@ -41,10 +45,20 @@ class Main {
     public static int menu() {
         System.out.println("Wciśnij:");
         System.out.println("1 - aby dodać studenta");
-        System.out.println("2 - aby wypisać wszystkich studentów");
+        System.out.println("2 - aby wpisać wiek i date urodzenia");
         System.out.println("3 - aby wyszukać studenta po imieniu");
         System.out.println("0 - aby wyjść z programu");
-        return scan.nextInt();
+          int wybor = -1;
+        try{
+            wybor = scan.nextInt();
+        }
+        catch(InputMismatchException e)
+            {
+            System.out.println("Błędny wybór");
+            scan.nextLine();
+        }
+            return wybor;
+        
     }
 
     public static String ReadName() throws WrongStudentName {
@@ -72,19 +86,27 @@ class Main {
                 System.out.println("Podaj poprawny wiek");
             }
             }
+            else{
+                scan.next();
+                System.out.println("Podaj poprawny wiek");
+                continue;
+            }
+        }
         scan.nextLine();
         System.out.println("Podaj datę urodzenia DD-MM-YYYY");
         String date = scan.nextLine();
         (new Service()).addStudent(new Student(name, age, date));
     }
     
-    public static void exercise2() throws IOException {        
-            Collection<Student> students = (new Service()).getStudents();
-            for(Student current : students) {
-                System.out.println(current.ToString());
+        public static void exercise2() throws IOException {        
+                Collection<Student> students = (new Service()).getStudents();
+                for(Student current : students) {
+                    System.out.println(current.ToString());
+            }
         }
-    }
 
+
+        
     public static void exercise3() throws IOException {
         scan.nextLine();
         System.out.println("Podaj imię: ");
@@ -96,5 +118,9 @@ class Main {
             System.out.println("Znaleziono: ");
             System.out.println(wanted.ToString());
         }
+    }
+    public static void exercise4() throws IOException{
+        scan.nextLine();
+        
     }
 }
